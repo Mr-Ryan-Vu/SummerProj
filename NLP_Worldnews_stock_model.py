cc75529ffd39f43ed2457b48c386732abdfdbec4
Arthur Thach <acthach@Arthurs-MacBook-Pro.local>
2023-11-11 18:34:16 -0800

    functions for text pre process

--- NLP_Worldnews_stock_model.py
+++ NLP_Worldnews_stock_model.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 #for text pre-processing
 import re, string
 import nltk
@@ -35,8 +36,47 @@ rows_to_keep = [*range(0, index[0]+1, 1)]
 
 #rows_to_keep = [whole_dataset[0:index[0]]]
 df_train= pd.read_csv('Input/Combined_News_DJIA.csv', header=0, skiprows = lambda x: x not in rows_to_keep)
-print("TRAINTRAINTRAINTRAIN:", df_train)
+print( df_train)
 df_test = pd.read_csv('Input/Combined_News_DJIA.csv', header=0, skiprows = lambda x: x in rows_to_keep)
-print("TESTTESTTEST:", df_test)
+print ( df_test)
 
 
+#convert to lowercase, strip and remove punctuations
+def preprocess(text):
+    text = text.lower() 
+    text=text.strip()  
+    text=re.compile('<.*?>').sub('', text) 
+    text = re.compile('[%s]' % re.escape(string.punctuation)).sub(' ', text)  
+    text = re.sub('\s+', ' ', text)  
+    text = re.sub(r'\[[0-9]*\]',' ',text) 
+    text=re.sub(r'[^\w\s]', '', str(text).lower().strip())
+    text = re.sub(r'\d',' ',text) 
+    text = re.sub(r'\s+',' ',text) 
+    return text
+
+ 
+# STOPWORD REMOVAL
+def stopword(string):
+    a= [i for i in string.split() if i not in stopwords.words('english')]
+    return ' '.join(a)
+#LEMMATIZATION
+# Initialize the lemmatizer
+wl = WordNetLemmatizer()
+ 
+# This is a helper function to map NTLK position tags
+def get_wordnet_pos(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+# Tokenize the sentence
+def lemmatizer(string):
+    word_pos_tags = nltk.pos_tag(word_tokenize(string)) # Get position tags
+    a=[wl.lemmatize(tag[0], get_wordnet_pos(tag[1])) for idx, tag in enumerate(word_pos_tags)] # Map the position tag and lemmatize the word/token
+    return " ".join(a)
